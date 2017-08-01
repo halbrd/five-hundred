48bb2469fc1fb8e4985659ecfefd52cff005a7f8
Will Holt <halbrd@gmail.com>
2017-08-01 14:37:21 +1000

    add missing code to BidValue constructor

--- five-hundred/five-hundred.py
+++ five-hundred/five-hundred.py
@@ -66,6 +66,8 @@ class BidValue:
 	def __init__(self, value):
 		if not value in BidValue.values:
 			raise ValueError(f'"{value}" is not a valid bid value')
+		else:
+			self.value = value
 
 	def to_string(self):
 		return str(self.value)
