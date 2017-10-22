4b4d2d4ae29ef910b4c7d1fb9dd2f46ab0aef502
Will Holt <halbrd@gmail.com>
2017-10-22 12:38:58 +1100

    implement Bid ordinal comparators and change player to player_id

--- five-hundred/bid.py
+++ five-hundred/bid.py
@@ -41,9 +41,9 @@ class BidValue:
 		return not self.__eq__(other)
 
 class BidSuit:
-	suits = ['CLUBS', 'DIAMONDS', 'HEARTS', 'SPADES', 'NO_TRUMPS', 'MISERE', 'OPEN_MISERE', 'PASS']
 	dependent_suits = ['CLUBS', 'DIAMONDS', 'HEARTS', 'SPADES', 'NO_TRUMPS']
 	independent_suits = ['MISERE', 'OPEN_MISERE', 'PASS']
+	suits = dependent_suits + independent_suits
 
 	def __init__(self, suit):
 		if not type(suit) == str:
@@ -83,10 +83,10 @@ class BidSuit:
 		return not self.__eq__(other)
 
 class Bid:
-	def __init__(self, input1, input2=None, player=None):
+	def __init__(self, input1, input2=None, player_id=None):
 		# argument names can't be specific (eg. value, suit) to remain accurate with flexible inputs
 		# eg. ('6', 'NO_TRUMPS'), ('MISERE')
-		self.player = player
+		self.player_id = player_id
 
 		if input1 in BidSuit.independent_suits and input2 is None:
 			self.suit = BidSuit(input1)
@@ -98,9 +98,9 @@ class Bid:
 
 	def to_string(self):
 		if self.suit in BidSuit.independent_suits:
-			return f'{self.player}: {self.suit}'
+			return f'{self.player_id}: {self.suit}'
 		else:
-			return f'{self.player}: {self.value} {self.suit}'
+			return f'{self.player_id}: {self.value} {self.suit}'
 
 	def to_minimal_string(self):
 		if self.suit in BidSuit.independent_suits:
@@ -112,7 +112,7 @@ class Bid:
 		return self.to_string()
 
 	def __repr__(self):
-		return f'Bid(player={self.player}, {self.value.value} {self.suit.suit})'
+		return f'Bid(player_id={self.player_id}, {self.value.value} {self.suit.suit})'
 
 	def __eq__(self, other):
 		return type(other) == Bid and self.value == other.value and self.suit == other.suit
@@ -121,10 +121,66 @@ class Bid:
 		return not self.__eq__(other)
 
 	def strict_equals(self, other):
-		return type(other) == Bid and self.player == other.player and self.value == other.value and self.suit == other.suit
+		return type(other) == Bid and self.player_id == other.player_id and self.value == other.value and self.suit == other.suit
+
+	def _compare(self, other):
+		hierarchy = [
+			Bid('6', 'SPADES'), Bid('6', 'CLUBS'), Bid('6', 'DIAMONDS'), Bid('6', 'HEARTS'), Bid('6', 'NO_TRUMPS'),
+			Bid('7', 'SPADES'), Bid('7', 'CLUBS'), Bid('7', 'DIAMONDS'), Bid('7', 'HEARTS'), Bid('7', 'NO_TRUMPS'),
+			Bid('MISERE'),
+			Bid('8', 'SPADES'), Bid('8', 'CLUBS'), Bid('8', 'DIAMONDS'), Bid('8', 'HEARTS'), Bid('8', 'NO_TRUMPS'),
+			Bid('9', 'SPADES'), Bid('9', 'CLUBS'), Bid('9', 'DIAMONDS'), Bid('9', 'HEARTS'), Bid('9', 'NO_TRUMPS'),
+			Bid('10', 'SPADES'), Bid('10', 'CLUBS'), Bid('10', 'DIAMONDS'),
+			Bid('OPEN_MISERE'),
+			Bid('10', 'HEARTS'), Bid('10', 'NO_TRUMPS')
+		]
+
+		if not self in hierarchy:
+			raise ValueError(repr(self) + ' is not a valid Bid')
+
+		if not other in hierarchy:
+			raise ValueError(repr(other) + ' is not a valid Bid')
+
+		# I don't like using string codes, but if you have a better idea for how to represent ternary values, I'm all ears
+		if hierarchy.index(self) > hierarchy.index(other):
+			return '>'
+		elif hierarchy.index(self) < hierarchy.index(other):
+			return '<'
+		else:
+			return '='
 
 	def __gt__(self, other):
-		pass # TODO
+		if other is None:   # any bid is bigger than no bid
+			return True
+
+		if not type(other) == Bid:
+			raise TypeError(f'\'>\' not supported between instances of \'Bid\' and \'{type(other)}\'')
+
+		return self._compare(other) == '>'
 
 	def __lt__(self, other):
-		pass # TODO
+		if other is None:   # any bid is bigger than no bid
+			return False
+
+		if not type(other) == Bid:
+			raise TypeError(f'\'<\' not supported between instances of \'Bid\' and \'{type(other)}\'')
+
+		return self._compare(other) == '<'
+
+	def __ge__(self, other):
+		if other is None:   # any bid is bigger than no bid
+			return True
+
+		if not type(other) == Bid:
+			raise TypeError(f'\'>=\' not supported between instances of \'Bid\' and \'{type(other)}\'')
+
+		return self._compare(other) in { '>', '=' }
+
+	def __le__(self, other):
+		if other is None:   # any bid is bigger than no bid
+			return False
+
+		if not type(other) == Bid:
+			raise TypeError(f'\'<=\' not supported between instances of \'Bid\' and \'{type(other)}\'')
+
+		return self._compare(other) in { '<', '=' }
