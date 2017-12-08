883be18215a5b44837deab2f9e3762d2fb5945a4
Will Holt <halbrd@gmail.com>
2017-12-08 22:03:10 +1100

    clarify variable names in trick.py

--- five-hundred/trick.py
+++ five-hundred/trick.py
@@ -1,11 +1,11 @@
 class Trick:
-	def __init__(self, leader):
-		self.leader = leader
+	def __init__(self, leader_id):
+		self.leader_id = leader_id
 		self.cards = []
 
 	def __str__(self):
-		return f'Leader: {self.leader}\n' \
+		return f'Leader: {self.leader_id}\n' \
 			+ 'Cards: ' + ', '.join([str(card) for card in self.cards])
 
 	def __repr__(self):
-		return f'Trick(leader={self.leader}, cards=[' + ', '.join([card.to_minimal_str() for card in self.cards]) + '])'
+		return f'Trick(leader={self.leader_id}, cards=[' + ', '.join([card.to_minimal_str() for card in self.cards]) + '])'
